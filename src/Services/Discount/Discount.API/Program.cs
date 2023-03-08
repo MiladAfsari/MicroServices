2dc4ba06338622d03cff8818415f8b3783151737
Milad <miliajax69@gmail.com>
2023-03-08 02:08:21 -0800

    Add Discount API

--- src/Services/Discount/Discount.API/Program.cs
+++ src/Services/Discount/Discount.API/Program.cs
@@ -1,6 +1,10 @@
+using Discount.API.Extensions;
+using Discount.API.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddScoped<IDiscountRepository, DiscountRepository>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -20,4 +24,5 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+app.MigrateDatabase<Program>();
 app.Run();
