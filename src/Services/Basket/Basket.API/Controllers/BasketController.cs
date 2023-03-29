76ed90840fba6d95dab70d56a850ced5a4c83d85
Milad <miliajax69@gmail.com>
2023-03-29 06:18:14 -0700

    Consume Discount GRPC Service

--- src/Services/Basket/Basket.API/Controllers/BasketController.cs
+++ src/Services/Basket/Basket.API/Controllers/BasketController.cs
@@ -1,4 +1,5 @@
 ﻿using Basket.API.Entities;
+using Basket.API.GrpcServices;
 using Basket.API.Repositories;
 using Microsoft.AspNetCore.Mvc;
 using System.Net;
@@ -10,11 +11,13 @@ namespace Basket.API.Controllers
     public class BasketController : ControllerBase
     {
         private readonly IBasketRepository _repository;
-
-        public BasketController(IBasketRepository repository)
+        private readonly DiscountGrpcService _discountGrpcService;
+        public BasketController(IBasketRepository repository, DiscountGrpcService discountGrpcService)
         {
             _repository = repository;
+            _discountGrpcService = discountGrpcService;
         }
+
         [HttpGet("{userName}", Name = "GetBasket")]
         [ProducesResponseType(typeof(ShopingCart), 200)]
         public async Task<ActionResult<ShopingCart>> GetBasket(string userName)
@@ -26,6 +29,13 @@ namespace Basket.API.Controllers
         [ProducesResponseType(typeof(ShopingCart), 200)]
         public async Task<ActionResult<ShopingCart>> UpdateBasket([FromBody] ShopingCart basket)
         {
+            //Consume Discount GRPC
+            foreach (var item in basket.Items)
+            {
+                var coupon = await _discountGrpcService.GetDiscount(item.ProductName);
+
+                item.Price -= coupon.Amount;
+            }
             return Ok(await _repository.UpdateBasket(basket));
         }
         [HttpDelete("{userName}", Name = "DeleteBasket")]
